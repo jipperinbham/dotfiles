3a72b03e921e61f732ca2f31aefb8ac6fae993d9
jipperinbham <jonphill9@gmail.com>
2021-11-24 14:40:05 -0600

    add a bunch of aliases and PATH additions

---
+++ git/aliases.zsh
@@ -0,0 +1,15 @@
+alias gs='git status -sb'
+
+alias ga='git add . --all'
+
+alias gcm='git commit -S -m'
+
+alias gcam='git commit -a --amend -S -m'
+
+alias gd='git diff -w --color=always'
+
+alias gm='git checkout main && git pull origin main'
+
+alias gmm='git checkout master && git pull origin master'
+
+alias gl='git pull --prune'
