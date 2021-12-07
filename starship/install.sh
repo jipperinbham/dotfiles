3807763eefd133d179c5eaeb43a2fa43713987be
jipperinbham <jonphill9@gmail.com>
2021-12-07 15:39:52 -0600

    fix type HOMD -> HOME

--- starship/install.sh
+++ starship/install.sh
@@ -2,6 +2,6 @@
 
 STARSHIP_DOTFILES_ROOT="$HOME/.dotfiles/starship"
 
-if ! [[ -f  "${HOMD}/.config/starship.toml" ]]; then
+if ! [[ -f  "${HOME}/.config/starship.toml" ]]; then
     ln -s "${STARSHIP_DOTFILES_ROOT}/starship.toml" "${HOME}/.config/starship.toml"
 fi
